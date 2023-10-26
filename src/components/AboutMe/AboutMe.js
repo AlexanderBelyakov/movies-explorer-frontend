b1edeabee817377c11b0368d407d18d6aaed386d
Alexander Belyakov <belyakov-alexander-2017@yandex.ru>
2023-10-26 19:39:43 +0300

    test

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -9,7 +9,7 @@ export function AboutMe() {
                 <div className="aboutme__info">
                     <p className="aboutme__name">Александр</p>
                     <p className="aboutme__profession">Фронтенд-разработчик, 23 года</p>
-                    <p className="aboutme__bio">Я родился и живу в НН, закончил факультет экономики и управления НГТУ. У меня нет ни жены 
+                    <p className="aboutme__bio">Я родился и живу в Нижнем, закончил факультет экономики и управления НГТУ. У меня нет ни жены 
 ни дочери. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
                     <a className="aboutme__link" href="https://github.com/AlexanderBelyakov?tab=repositories" target="_blank" rel="noreferrer">Github</a>
                 </div>
